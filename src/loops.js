06c97d378a13c366b1a6b7ff8bbcdf044ae8932f
Kyle Maune <kmaune71@gmail.com>
2014-11-07 17:03:14 -0800

    Update loops.js

--- src/loops.js
+++ src/loops.js
@@ -23,6 +23,21 @@ a.forEach(function(entry) {
     console.log(entry);
 });
 
+// for-in loops through the enumerable properties of an object, not the indexes of an array.
+var key;
+var a = [];
+a[0] = "a";
+a[10] = "b";
+a[10000] = "c";
+for (key in a) {
+    if (a.hasOwnProperty(key)  &&        // These are explained
+        /^0$|^[1-9]\d*$/.test(key) &&    // and then hidden
+        key <= 4294967294                // away below
+        ) {
+        console.log(a[key]);
+    }
+}
+
 // The while loop is used when you want the loop to execute and continue executing while the specified condition is true. 
 // Example:
 var i=0
