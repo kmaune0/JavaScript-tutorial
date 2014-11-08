5de8bfbd4f3133c422f43dc9d0bddbbc47d9f790
Kyle Maune <kmaune71@gmail.com>
2014-11-07 16:57:31 -0800

    Update loops.js

--- src/loops.js
+++ src/loops.js
@@ -4,12 +4,20 @@
 // Loops in JavaScript are used to execute the same block of code a specified number of times or while a specified condition is true.
 // 2 kinds of loops: for and while
 // The for loop is used when you know in advance how many times the script should run.
-// Example:
+// Examples:
 var i=0
 for (i=0;i<=10;i++) {
   console.log("The number is " + i);
   console.log("<br />");
 }
+
+var index;
+var a = ["a", "b", "c"];
+for (index = 0; index < a.length; ++index) {
+    console.log(a[index]);
+}
+
+// If you're using an environment that supports the Array features of ECMAScript5 (directly or using a shim), you can use the new forEach function:
 
 // The while loop is used when you want the loop to execute and continue executing while the specified condition is true. 
 // Example:
